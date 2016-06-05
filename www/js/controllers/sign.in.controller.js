d25a35dc3f7f9e21a64f56c98f1295936ff0d125
ZlMagda <magda.m.zielinska@gmail.com>
2016-06-05 21:42:08 +0200

    first commit

---
+++ www/js/controllers/sign.in.controller.js
@@ -0,0 +1,55 @@
+angular.module('sign.in.controllers', [])
+  .controller('SignInCtrl', function ($timeout, $scope, $state, $window,$http) {
+
+
+
+    $scope.signIn = function (email, password) {
+
+     /* ReceiptsServer.loginUser(email, password).then(function (response) {
+
+        if (response.data != '"Login or password is incorrect."') {
+
+          $window.sessionStorage.token = response.data.token;
+          DefService.goTo('tab.newReceipt');
+
+        } else {
+          DefService.messagesMaker(response.data);
+        }
+      }, function (error) {
+
+        console.log(error);
+      });*/
+
+      var dataToPost = 'email=' + email + '&password=' + password;
+
+      var req = {
+        method: 'POST',
+        url: 'https://walletapplication.herokuapp.com/api/local/login',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+          "Access-Control-Max-Age": "3600",
+          "Access-Control-Allow-Headers": "x-requested-with"},
+        data: dataToPost
+      };
+
+
+      $http(req).then(function (response) {
+
+        if (response.data != '"Login or password is incorrect."') {
+
+          $window.sessionStorage.token = response.data.token;
+
+          $state.go("tab.dash")
+          //DefService.goTo('tab.newReceipt');
+
+        } else {
+          ///DefService.messagesMaker(response.data);
+        }
+      }, function (error) {
+
+        console.log(error);
+      });
+    };
+
+  });
